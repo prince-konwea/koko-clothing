e82ca967c47bd1becbfbd82e0ae50f854594bf48
Prince Nkem Konwea <kokodecontee123@gmail.com>
2022-08-23 22:46:15 -0700

    created directory and menu-item components in project

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+import MenuItem from "../menu-item/menu-item.component";
+
+import "./directory.styles.scss"
+
+class Directory extends React.Component {
+   constructor(){
+    super();
+
+this.state = {
+    sections: [
+        {
+          title: 'hats',
+          imageUrl: 'https://images.pexels.com/photos/936038/pexels-photo-936038.jpeg?auto=compress&cs=tinysrgb&w=600',
+          id: 1,
+          linkUrl: 'shop/hats'
+        },
+        {
+          title: 'jackets',
+          imageUrl: 'https://media.istockphoto.com/photos/portrait-of-pensive-handsome-man-in-eyeglasses-looking-away-isolated-picture-id900882774?k=20&m=900882774&s=612x612&w=0&h=LxOHs2OcqP1R6F6Ow7iUeuz7JDdkfid89SqRTr4qOi4=',
+          id: 2,
+          linkUrl: 'shop/jackets'
+        },
+        {
+          title: 'sneakers',
+          imageUrl: 'https://images.pexels.com/photos/4273288/pexels-photo-4273288.jpeg?auto=compress&cs=tinysrgb&w=600',
+          id: 3,
+          linkUrl: 'shop/sneakers'
+        },
+        {
+          title: 'womens',
+          imageUrl: 'https://images.pexels.com/photos/1839904/pexels-photo-1839904.jpeg?auto=compress&cs=tinysrgb&w=600',
+          size: 'large',
+          id: 4,
+          linkUrl: 'shop/womens'
+        },
+        {
+          title: 'mens',
+          imageUrl: 'https://images.pexels.com/photos/1342609/pexels-photo-1342609.jpeg?auto=compress&cs=tinysrgb&w=600',
+          size: 'large',
+          id: 5,
+          linkUrl: 'shop/mens'
+        }
+      ]
+}
+
+   }
+   render(){
+    return(
+    <div className="directory-menu">
+       {
+          this.state.sections.map(({title, imageUrl, id, size}) => (
+            <MenuItem key={id} title={title} imageUrl={imageUrl}  size={size}/>
+          ))
+       }
+    </div>
+    )
+   }
+}
+
+export default Directory;
