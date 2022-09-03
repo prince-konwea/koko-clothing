7eded061fc458a8919790f3eb1293a2eceb92f95
Prince Nkem Konwea <kokodecontee123@gmail.com>
2022-09-03 22:00:42 +0100

    created directory component reducer and directory selector files to migrate our local state to redux

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,62 +1,26 @@
 import React from "react";
 
 import MenuItem from "../menu-item/menu-item.component";
+import { connect } from "react-redux";
+import { selectDirectorySections } from "../../redux/directory/directory.selectors";
+import { createStructuredSelector } from "reselect";
 
 import "./directory.styles.scss"
 
-class Directory extends React.Component {
-   constructor(){
-    super();
-
-this.state = {
-    sections: [
-        {
-          title: 'hats',
-          imageUrl: 'https://images.pexels.com/photos/936038/pexels-photo-936038.jpeg?auto=compress&cs=tinysrgb&w=600',
-          id: 1,
-          linkUrl: 'shop/hats'
-        },
-        {
-          title: 'jackets',
-          imageUrl: 'https://media.istockphoto.com/photos/portrait-of-pensive-handsome-man-in-eyeglasses-looking-away-isolated-picture-id900882774?k=20&m=900882774&s=612x612&w=0&h=LxOHs2OcqP1R6F6Ow7iUeuz7JDdkfid89SqRTr4qOi4=',
-          id: 2,
-          linkUrl: 'shop/jackets'
-        },
-        {
-          title: 'sneakers',
-          imageUrl: 'https://images.pexels.com/photos/4273288/pexels-photo-4273288.jpeg?auto=compress&cs=tinysrgb&w=600',
-          id: 3,
-          linkUrl: 'shop/sneakers'
-        },
-        {
-          title: 'womens',
-          imageUrl: 'https://images.pexels.com/photos/1839904/pexels-photo-1839904.jpeg?auto=compress&cs=tinysrgb&w=600',
-          size: 'large',
-          id: 4,
-          linkUrl: 'shop/womens'
-        },
-        {
-          title: 'mens',
-          imageUrl: 'https://images.pexels.com/photos/1342609/pexels-photo-1342609.jpeg?auto=compress&cs=tinysrgb&w=600',
-          size: 'large',
-          id: 5,
-          linkUrl: 'shop/mens'
-        }
-      ]
-}
-
-   }
-   render(){
-    return(
+const Directory = ({ sections}) => {
+   
     <div className="directory-menu">
        {
-          this.state.sections.map(({id, ...otherSectionProps}) => (
+          sections.map(({id, ...otherSectionProps}) => (
             <MenuItem key={id} {...otherSectionProps}/>
           ))
        }
     </div>
-    )
+    
    }
-}
 
-export default Directory;
+   const mapStateToProps = createStructuredSelector({
+    sections: selectDirectorySections
+   })
+
+export default connect(mapStateToProps)(Directory);
